a9b9d993d5022c94e1d0b02e6be127f0572188fd
tacheraSasi <tacherasasi@gmail.com>
2026-02-24 15:56:51 +0300

    feat(greetings-card): refactor GreetingsCard component and update Landing page layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
 const nameInput = document.getElementById("Name");
 const generateBtn = document.getElementById("generateBtn");
+const mainDiv = document.getElementById("main-div");
+
+generateBtn.addEventListener("click", async () => {
+  const name = nameInput.value.trim();
+  if (!name) {
+    alert("Please enter a name.");
+    return;
+  }
+});
